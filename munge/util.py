2cb4293ca2dac2a20006ec3a7b204a4f35edcfaf
Matt Griswold <grizz@20c.com>
2016-01-04 04:14:33 +0000

    fix recursive updates dropping kwargs

--- munge/util.py
+++ munge/util.py
@@ -3,27 +3,36 @@ import collections
 from copy import deepcopy
 
 
-def recursive_update(a, b, copy=False, merge_lists=True):
+def recursive_update(a, b, **kwargs):
     """
     recursive dict a.update(b), merges dicts and lists
     Note: will clobber non dict keys if b has a dict with same key
+    options:
+        copy: deepcopy instead of reference (default False)
+        merge_lists: merge lists as well (default True)
     """
-    if copy:
-        a = deepcopy(a)
+    copy = kwargs.get('copy', False)
+    merge_lists = kwargs.get('merge_lists', True)
 
     for k, v in b.iteritems():
         if isinstance(v, collections.Mapping):
             if isinstance(a.get(k, None), collections.Mapping):
-                recursive_update(a[k], v)
+                recursive_update(a[k], v, **kwargs)
                 continue
 
         elif merge_lists and type(v) is list:
             if type(a.get(k, None)) is list:
-                a[k].extend(v)
+                if copy:
+                    a[k].extend(deepcopy(v))
+                else:
+                    a[k].extend(v)
                 continue
 
         # warning, clobbering
-        a[k] = v
+        if copy:
+            a[k] = deepcopy(v)
+        else:
+            a[k] = v
 
     return a
 
--- tests/test_util.py
+++ tests/test_util.py
@@ -37,22 +37,24 @@ def test_recursive_update():
     assert merged == a
 
 def test_recursive_update_copy():
+    refdict = {'z': 1, 'y': 2}
     d0 = {'a': {'b': {'x': '1', 'y': '2'}}}
-    d1 = {'a': {'c': {'gg': {'m': '3'}, 'xx': '4'}}}
+    d1 = {'a': {'c': {'gg': {'m': '3', 'ref': refdict}, 'xx': '4'}}}
 
-    r0 = {'a': {'b': {'x': '1', 'y': '2'}, 'c': {'gg': {'m': '3'}, 'xx': '4'}}}
+    r0 = {'a': {'b': {'x': '1', 'y': '2'}, 'c': {'gg': {'m': '3', 'ref': {'z': 1, 'y': 2}}, 'xx': '4'}}}
 
-    d0cp = copy.deepcopy(d0)
     res = munge.util.recursive_update(d0, d1, copy=True)
     assert r0 == res
-    assert d0cp == d0
-    assert r0 != d0
+
+    refdict['z'] = 42
+    assert r0 == res
 
 def test_recursive_update_lists():
     a = {
         'a': 1,
         'd': {
             'a': 1,
+            'l2': [1, 2, 3],
             },
         'l': [1, 2, 5],
         'l2': [1, 2, 3],
@@ -62,6 +64,7 @@ def test_recursive_update_lists():
         'd': {
             'a': 2,
             'c': 3,
+            'l2': [4, 5],
             },
         'l2': [4, 5],
         'l3': [1, 2],
@@ -72,6 +75,7 @@ def test_recursive_update_lists():
         'd': {
             'a': 2,
             'c': 3,
+            'l2': [4, 5],
             },
         'l': [1, 2, 5],
         'l2': [4, 5],
