ddebdac91a19681ff229d8aaec5d53f3b122542b
Matt Griswold <grizz@20c.com>
2021-06-17 22:33:43 -0500

    stop isort from changing import order for toml

--- src/munge/codec/toml.py
+++ src/munge/codec/toml.py
@@ -5,7 +5,7 @@ try:
     if os.environ.get("MUNGE_TOML_LIBRARY", None):
         importlib.import_module(f"munge.codec.toml_{os.environ['MUNGE_TOML_LIBRARY']}")
     else:
-        import munge.codec.toml_tomlkit  # noqa
+        import munge.codec.toml_tomlkit  # noqa isort:skip
         import munge.codec.toml_toml  # noqa
 
 except ValueError as exc:
