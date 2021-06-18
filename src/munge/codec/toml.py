5cc66b2c101dc4aba7109019d3c7aceb54f7187e
Matt Griswold <grizz@20c.com>
2021-06-17 22:29:38 -0500

    tomlkit first, still

--- src/munge/codec/toml.py
+++ src/munge/codec/toml.py
@@ -5,8 +5,8 @@ try:
     if os.environ.get("MUNGE_TOML_LIBRARY", None):
         importlib.import_module(f"munge.codec.toml_{os.environ['MUNGE_TOML_LIBRARY']}")
     else:
-        import munge.codec.toml_toml  # noqa
         import munge.codec.toml_tomlkit  # noqa
+        import munge.codec.toml_toml  # noqa
 
 except ValueError as exc:
     # don't load both toml modules
